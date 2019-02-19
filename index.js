4caf0d67f18817a4d19c081da15cd2153c43fbdc
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-02-19 13:05:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+var arr = [];
+var i;
+
+function theBeatlesPlay(musicians,instruments){
+  for(i=0,i < musicians.length),i++) {
+    arr[i] = musicians[i] + instruments [i]
+    console.log(arr)
+  }
+}
